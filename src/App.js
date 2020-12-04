8eee3a55badd60733eda9cc1ace15d0208824446
Klimuk Dzmitry <dklimuk@exadel.net>
2020-12-04 18:05:19 +0300

    buttons, checkBox

--- src/App.js
+++ src/App.js
@@ -1,22 +1,52 @@
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import SaveIcon from '@material-ui/icons/Save';
+import CheckBox from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import { Box } from '@material-ui/core';
+
+
+
+const CheckExample = ({label}) => {
+  const [checked, setChecked] = useState(true);
+  return(
+    <FormControlLabel
+      control={
+        <CheckBox
+        checked={checked}
+        // onClick={() => {
+        //   setChecked( (checked) => !checked )
+        // }}
+        onChange={(e)=>setChecked(e.target.checked)}
+        />
+      }
+      label={label}
+    />
+    
+  )
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Box variant="contained" >
+          <CheckExample label="Test1" />          
+          <CheckExample label="Test2" />
+        </Box>
+        
+        <Button
+          startIcon = {<SaveIcon/>}
+          endIcon = {<SaveIcon/>}  
+          variant="contained" 
+          color="secondary"
+          style={{fontSize: '20px'}} 
+          size="large">
+          Hello World
+        </Button>
+        <img src={logo} className="App-logo" alt="logo" />        
       </header>
     </div>
   );
