f9e85a28d4847084890d1fc65e82b29d899c6e21
Klimuk Dzmitry <dklimuk@exadel.net>
2020-12-09 12:56:08 +0300

    classNames

--- src/CoolButton.js
+++ src/CoolButton.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core';
+import classNames from 'classnames'
+
 
 const useStyles = makeStyles(theme => ({
     buttonStyles: {
         margin: '20px 0',
+        padding: '20px',
         fontSize: cool => (cool ? '12px' : '18px'),
         color: "tomato",
         [theme.breakpoints.up('sm')]: {
@@ -20,5 +23,5 @@ const useStyles = makeStyles(theme => ({
 
 export default function Hook({color}){
     const classes = useStyles(color);
-    return <Button variant="contained" className={classes.buttonStyles, classes.buttonBackground}>Props Button</Button>
+    return <Button variant="contained" className={classNames(classes.buttonStyles, classes.buttonBackground)}>Props Button</Button>
 }
